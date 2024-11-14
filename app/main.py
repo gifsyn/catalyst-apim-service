89184248cc2bc2f3106f14d261cb4aea610fe472
gifsyn <gifsynpro@gmail.com>
2024-11-15 00:41:15 +0900

    Refactor main application to include routers and simplify health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,21 +1,12 @@
 from fastapi import FastAPI
 
-from app.schemas.health_check import HealthCheckResponse, HealthCheckStatus
-from app.settings import project_settings
+from app.api.routers import routers
+from app.core.settings import project_settings
 
 app = FastAPI(
     title=project_settings.name,
-    description=("Azure OpenAI Service、Amazon Bedrock、"
-                 "Google Cloud Vertex AIの統合APIサービス"),
+    description="Azure OpenAI Service、Amazon Bedrock、Google Cloud Vertex AIの統合APIサービス",
     version=project_settings.version,
 )
 
-@app.get("/", response_model = HealthCheckResponse)
-async def health_check() -> HealthCheckResponse:
-    """
-    サーバーが起動しているか確認する.
-    """
-
-    response = HealthCheckResponse(status=HealthCheckStatus.READY)
-
-    return response
+app.include_router(routers)
